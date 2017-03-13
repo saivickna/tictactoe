a071ce3e3b23cbfe2a3e70a8863d96a6acfb38c5
Saivickna Raveendran <saivickna.r@gmail.com>
2017-03-13 11:55:32 -0700

    Refactoring to use board as an object

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
 var playerTurn = true;
-var board = new Array(8);
+var gameBoard = new TicTacToeBoard();
 var gameOver = false;
 var switchPlayers = () => {
   playerTurn = !playerTurn;
   $('#player').text(playerTurn ? 'Player 1 Turn' : 'Player 2 Turn');
-}
-var checkBoard = (board, playerCheck) => {
-
-  if ((board[0] === playerCheck && board[1] === playerCheck && board[2] === playerCheck) ||
-      (board[3] === playerCheck && board[4] === playerCheck && board[5] === playerCheck) ||
-      (board[6] === playerCheck && board[7] === playerCheck && board[8] === playerCheck) ||
-      (board[0] === playerCheck && board[3] === playerCheck && board[6] === playerCheck) ||
-      (board[1] === playerCheck && board[4] === playerCheck && board[7] === playerCheck) ||
-      (board[2] === playerCheck && board[5] === playerCheck && board[8] === playerCheck) ||
-      (board[0] === playerCheck && board[4] === playerCheck && board[8] === playerCheck) ||
-      (board[2] === playerCheck && board[4] === playerCheck && board[6] === playerCheck)) {
-      return true;
-  }
-  return false;
-}
-var playerCanGo = (board) => {
-  for (var i = 0; i < board.length; i++) {
-    if (board[i] === undefined) {
-      return true;
-    }
-  }
-  return false;
 }
 var resetBoard = () => {
   for (var i = 0; i < 9; i++) {
@@ -37,18 +15,18 @@ $(document).ready(() => {
     if (gameOver) {
       resetBoard();
       gameOver = false;
-      board = new Array(8);
+      gameBoard.reset();
       switchPlayers();
     }
-    if (board[e.target.id] === undefined) {
+    if (gameBoard.checkDefined(e.target.id)) {
       $('#message').text('');
       $('#' + e.target.id).text(playerTurn ? 'X' : 'O');
-      board[e.target.id] = playerTurn ? 'X' : 'O';
-      if(checkBoard(board, playerTurn ? 'X' : 'O')) {
+      gameBoard.set(e.target.id, playerTurn ? 'X' : 'O');
+      if(gameBoard.checkBoard(playerTurn ? 'X' : 'O')) {
         $('#message').text(playerTurn ? 'Player 1 Wins' : 'Player 2 Wins');
         gameOver = true;
         $('#player').text('Click to Restart.');
-      } else if (!playerCanGo(board)) {
+      } else if (!gameBoard.playerCanGo()) {
         $('#message').text('Tie Game!');
         gameOver = true;
         $('#player').text('Click to Restart.');
---
+++ board.js
@@ -0,0 +1,34 @@
+function TicTacToeBoard () {
+  this.game = new Array(8);
+};
+TicTacToeBoard.prototype.checkBoard = function(playerCheck) {
+  var board = this.game;
+  if ((board[0] === playerCheck && board[1] === playerCheck && board[2] === playerCheck) ||
+    (board[3] === playerCheck && board[4] === playerCheck && board[5] === playerCheck) ||
+    (board[6] === playerCheck && board[7] === playerCheck && board[8] === playerCheck) ||
+    (board[0] === playerCheck && board[3] === playerCheck && board[6] === playerCheck) ||
+    (board[1] === playerCheck && board[4] === playerCheck && board[7] === playerCheck) ||
+    (board[2] === playerCheck && board[5] === playerCheck && board[8] === playerCheck) ||
+    (board[0] === playerCheck && board[4] === playerCheck && board[8] === playerCheck) ||
+    (board[2] === playerCheck && board[4] === playerCheck && board[6] === playerCheck)) {
+    return true;
+  }
+  return false;
+}
+TicTacToeBoard.prototype.reset = function() {
+  this.game = new Array(8);
+}
+TicTacToeBoard.prototype.checkDefined = function(i) {
+  return (this.game[i] === undefined);
+}
+TicTacToeBoard.prototype.set = function(i, player) {
+  this.game[i] = player;
+}
+TicTacToeBoard.prototype.playerCanGo = function() {
+  for (var i = 0; i < this.game.length; i++) {
+    if (this.game[i] === undefined) {
+      return true;
+    }
+  }
+  return false;
+}
